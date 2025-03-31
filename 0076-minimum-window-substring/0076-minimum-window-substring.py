7b11a8b4e8023016b8e4a4e8ffc701f56f77c229
Liben Adugna <152218324+Lib1221@users.noreply.github.com>
2025-03-31 20:32:47 +0300

    Time: 551 ms (10.23%), Space: 18.4 MB (35.59%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m, n = len(s), len(t)
+        counter_t = Counter(t)
+        window = Counter()
+
+        def check(first, second):
+            for char in second:
+                if first[char] < second[char]:
+                    return False
+            return True
+
+        left = 0
+        right = 0
+        ans = float("inf")
+        indexes = []
+
+        while right < m:
+            window[s[right]] += 1
+
+            while (right-left)+1 >= n and check(window, counter_t):
+                current_length = (right-left)+1
+                if current_length < ans:
+                    ans = current_length
+                    indexes = [left, right]
+                
+                window[s[left]] -= 1
+                left += 1
+
+            right += 1
+        
+        if ans == float("inf"):
+            return ""
+        
+        return s[indexes[0]: indexes[1]+1]
